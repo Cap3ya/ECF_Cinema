01252c434085eb1d8a6cc99cc474cc127428f905
capeya <capeya92@gmail.com>
2024-05-13 14:03:23 +0200

    Remove old files and Add new ones

--- config/init.php
+++ config/init.php
@@ -2,12 +2,9 @@
 // Initialisation de la session
 session_start();
 
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
 
 $loader = new \Twig\Loader\FilesystemLoader('view');
 $twig = new \Twig\Environment($loader);
-
 
 $twig->addGlobal('session', $_SESSION);
