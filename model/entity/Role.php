3a41c4e3ab2b9a4ecece25685a1c57997516fe71
eselene <eviougeas@gmail.com>
2024-05-15 10:44:55 +0200

    modif de classe : supp de

--- model/entity/Role.php
+++ model/entity/Role.php
@@ -6,19 +6,16 @@ class Role
 {
 
     private $id;
-    private $id_film;
     private $acteur;
     private $personnage;
 
     public function __construct(
         int $id = null,
-        int $id_film,
         Acteur $acteur, 
         string $personnage
     )
     {
         $this->setId($id);
-        $this->setId_film($id_film);
         $this->setActeur($acteur);
         $this->setPersonnage($personnage);
     }
@@ -51,23 +48,5 @@ class Role
     public function getActeur()
     {
         return $this->acteur;
-    }
-
-    /**
-     * Get the value of id_film
-     */ 
-    public function getId_film()
-    {
-        return $this->id_film;
-    }
-
-    /**
-     * Set the value of id_film
-     *
-     * @return  self
-     */ 
-    public function setId_film($id_film)
-    {
-        $this->id_film = $id_film;
     }
 }
