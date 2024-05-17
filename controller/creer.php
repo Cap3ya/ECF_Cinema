87c0ed8528c07f084bf67b509a26c816e6f4361c
ELNA Viougeas <157828151+eselene@users.noreply.github.com>
2024-05-17 21:18:32 +0200

    Update creer.php
    ajout de $message et try/catch

--- controller/creer.php
+++ controller/creer.php
@@ -7,21 +7,27 @@ use Model\repository\FilmDAO;
 
 $filmDAO = new FilmDAO();
 
-//récupérer les données de la twig pour insertion dans les tables: 1 film, 3 roles et peut être 3 acteurs
+//récupérer les données de la twig pour insertion dans les tables: 1 film, 1 oun plusieurs roles et peut être 1 ou plusieurs acteurs
 if (isset($_POST['titre']) && isset($_POST['realisateur']) && isset($_POST['affiche']) &&
     isset($_POST['annee']) && isset($_POST['acteur_nom']) && isset($_POST['acteur_prenom']) && isset($_POST['personnage'])) {
+    try {
+        //Récupérer Acteurs et Roles
+        $roles = [];
+        for ($i=0; $i < count($_POST['personnage']) ; $i++) { 
+            $acteur = new Acteur(0, $_POST['acteur_nom'][$i], $_POST['acteur_prenom'][$i]);
+            $role = new Role(0, $acteur, $_POST['personnage'][$i]);
+            $roles[] = $role;
+        }
 
-    //Récupérer Acteurs et Roles
-    $roles = [];
-    for ($i=0; $i < count($_POST['personnage']) ; $i++) { 
-        $acteur = new Acteur(0, $_POST['acteur_nom'][$i], $_POST['acteur_prenom'][$i]);
-        $role = new Role(0, $acteur, $_POST['personnage'][$i]);
-        $roles[] = $role;
-    }
-
-    $film = new Film(0, $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee'], $roles);
-
-    $filmDAO::addOne($film);
+        $film = new Film(0, $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee'], $roles);
+    
+        $filmDAO::addOne($film);
+        $message = "Création OK";
+    } catch (Exception $e) {
+        $message = "Erreur de création : " . $e->getMessage();
+    }        
 }
-
+else {
+    $message = null;
+}
 echo $twig->render('creer.html.twig');
